650eff045096587be9a978ce2b1ed557c7faa0bb
Arif Viansyah <127389870+13830@users.noreply.github.com>
2023-05-12 13:01:28 +0700

    Add files via upload

---
+++ UTSPBO/MahasiswaAktif.java
@@ -0,0 +1,21 @@
+package UTS_A11202113830;
+
+public class MahasiswaAktif extends Mahasiswa{
+
+	public MahasiswaAktif(String nim, String nama, int semester, int usia, String[] krs) {
+		super(nim, nama, semester, usia, krs);
+		// TODO Auto-generated constructor stub
+	}
+
+	 	float hitungRataNilai(int[] nilai) {
+	        return super.hitungRataNilai(nilai);
+	    }
+
+	    void infoMahasiswa() {
+	       super.infoMahasiswa();
+	    }
+
+	    void infoKrsMahasiswa() {
+	        super.infoKrsMahasiswa();
+	    }
+}
